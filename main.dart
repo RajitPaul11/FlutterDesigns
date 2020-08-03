4493ebade138de5fcc8378cfa7e7a3486f9b4cc3
Rajitpaul <rajitpaul09@gmail.com>
2020-08-03 16:08:33 +0530

    Initial Commit

---
+++ main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:layout_app/ui/home.dart';
+
+main()
+{
+  runApp(MyApp());
+}
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return myapp1();
+  }
+}
